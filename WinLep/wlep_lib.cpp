8ad3f8be9b5c716a34f53b3848d974de310a1e81
Roderik Ploszek <roderik.ploszek@gmail.com>
2021-04-19 23:02:20 +0200

    refactor: better variable name

--- WinLep/wlep_lib.cpp
+++ WinLep/wlep_lib.cpp
@@ -3,12 +3,12 @@
 #include "wlep_reader.h"
 #include "lepton_converter.h"
 
-std::vector<unsigned char> convertLeptonToJpg(const std::string jpg_filename) {
+std::vector<unsigned char> convertLeptonToJpg(const std::string wlep_filename) {
 	// TODO: This should be called just once, preferably in DllMain
 	const std::wstring module_dir_path = getModuleDirectory();
 
 	wlep::LeptonConverter converter(module_dir_path, g_lepton_exe, g_lepton_args);
-	wlep::WLepReader reader(jpg_filename);
+	wlep::WLepReader reader(wlep_filename);
 	std::vector<uChar> lepton_data = reader.validateFileAndReadLeptonData();
 	std::vector<unsigned char> jpeg_data = converter.convertLeptonToJpg(lepton_data);
 	return jpeg_data;
